4f663b1a5f3cf5e81455294949c7a7bc2202f2f0
debruw <ebru_cakar_11@hotmail.com>
2018-09-06 11:14:37 +0300

    First init

---
+++ mail/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 587;
+            sc.Host = "smtp.gmail.com";
+            sc.EnableSsl = true;
+
+            string kime = textBox1.Text;
+            string konu = textBox2.Text;
+            string icerik = textBox3.Text;
+
+            sc.Credentials = new NetworkCredential("debruwc@gmail.com", "********");
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("debruwc@gmail.com", "Ebru Ç.");
+            mail.To.Add(kime);
+
+            mail.Subject = konu;
+            mail.IsBodyHtml = true;
+            mail.Body = icerik;
+            sc.Send(mail);
+        }
+    }
+}
